36ffb71ba9679c14b3d4b4f3698870027af659e8
Hugo <le19080710@itemrida.edu.mx>
2023-10-09 14:23:21 -0600

    add client and server modules

---
+++ server.py
@@ -0,0 +1,55 @@
+import socket
+import sys
+
+server_ip = str(sys.argv[1])
+port = 8000
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # bind the socket to a specific address and port
+server.bind((server_ip, port))
+    # listen for incoming connections
+server.listen(0)
+print(f"Listening on {server_ip}:{port}")
+
+    # accept incoming connections
+client_socket, client_address = server.accept()
+print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
+
+
+def run_server():
+    # create a socket object
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    
+
+    # receive data from the client
+    while True:
+        request = client_socket.recv(1024)
+        request = request.decode("utf-8") # convert bytes to string
+        
+        # if we receive "close" from the client, then we break
+        # out of the loop and close the conneciton
+        if request.lower() == "close":
+            # send response to the client which acknowledges that the
+            # connection should be closed and break out of the loop
+            client_socket.send("closed".encode("utf-8"))
+            break
+
+        print(f"Received: {request}")
+        response = input("Enter respose: ")
+        if response.lower() == "close":
+            break
+        response = response.encode("utf-8")
+        
+
+
+            # convert string to bytes
+        # convert and send accept response to the client
+        client_socket.send(response)
+
+    # close connection socket with the client
+    client_socket.close()
+    print("Connection to client closed")
+    # close server socket
+    server.close()
+run_server()
+
